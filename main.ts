4e82a8b5f19862ec01455b0f719693ec44161da7
lucas-nguyen0 <lucas.nguyen@mths.ca>
2024-11-21 10:34:00 -0500

    program counts down from 4 and displays it on Neopixels

--- main.ts
+++ main.ts
@@ -1,8 +1,38 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
  * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created on: Oct 2024
+ * This program counts down from 4 and displays it on Neopixels.
 */
 
-basic.showString('Hello, World!')
+let neopixelStrip: neopixel.Strip = null
+let countdownNumber: number
+
+basic.clearScreen()
+neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.show()
+countdownNumber = 4
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function() {
+  countdownNumber = 4
+  neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Purple))
+  neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Purple))
+  neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Purple))
+  neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Purple))
+  neopixelStrip.show()
+
+  while (countdownNumber >= 0){
+    basic.showNumber(countdownNumber)
+    neopixelStrip.show()
+    countdownNumber = (countdownNumber - 1)
+    neopixelStrip.setPixelColor(countdownNumber, neopixel.colors(NeoPixelColors.Black))
+    basic.pause(500)
+  }
+  basic.clearScreen()
+  basic.showIcon(IconNames.Happy)
+})
